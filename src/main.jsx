772951c6529b683f203e7f86008b0e7d61777918
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-06-19 22:29:57 +0300

    Creating All Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react';
 import './index.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  // Wrapping the App component with BrowserRouter to enable routing
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
 
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
+  
 )
