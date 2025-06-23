d16222e287765e2fb33094b2312e09c026d2c8bc
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-06-23 15:48:51 +0300

    Update MovieDetails & FeaturedSection.jsx Comp

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -23,7 +23,7 @@ const FeaturedSection = () => {
       </div>
 
       <div className='flex flex-wrap max-sm:justify-center items-center gap-8 mt-8'>
-        {dummyShowsData.slice(0, 5).map((show)=> (
+        {dummyShowsData.slice(0, 4).map((show)=> (
             <MovieCard key={show._id} movie={show}/>
         ))}
       </div>
