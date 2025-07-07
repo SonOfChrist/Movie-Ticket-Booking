a6d8e6b1c610e3f477cdfdf4dd08c68dffd3eac6
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-07-07 14:18:33 +0300

    Created Movies/Favorite.jsx

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -8,8 +8,8 @@ const Favorite = () => {
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
       <BlurCircle top='150px' left='0px' />
       <BlurCircle bottom='50px' right='50px' />
-      <h1 className='text-lg font-medium my-4'>Your Favorite Movies</h1>
-      <div className='flex flex-wrap max-sm:justify-center gap-8'>
+      <h1 className='text-lg font-medium my-4'>Thanks for watching Movies with Us, Here are your Favorites</h1>
+      <div className='flex flex-wrap max-sm:justify-center gap-14'>
         {dummyShowsData.map((movie) => (
           <MovieCard key={movie._id} movie={movie} />
         ))}
@@ -17,7 +17,7 @@ const Favorite = () => {
     </div>
   ) : (
     <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-3xl font-bold text-center'>No Movies Available</h1>
+      <h1 className='text-3xl font-bold text-center'>No Movies Available at the moment</h1>
     </div>
   )
 }
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -8,8 +8,8 @@ const Movies = () => {
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
       <BlurCircle top='150px' left='0px' />
       <BlurCircle bottom='50px' right='50px' />
-      <h1 className='text-lg font-medium my-4'>Now Showing</h1>
-      <div className='flex flex-wrap max-sm:justify-center gap-8'>
+      <h1 className='text-lg font-medium my-4'>Now Showing | Buy Your Tickect, Don't miss Out</h1>
+      <div className='flex flex-wrap max-sm:justify-center gap-14'>
         {dummyShowsData.map((movie) => (
           <MovieCard key={movie._id} movie={movie} />
         ))}
@@ -17,7 +17,7 @@ const Movies = () => {
     </div>
   ) : (
     <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-3xl font-bold text-center'>No Movies Available</h1>
+      <h1 className='text-3xl font-bold text-center'>No Movies Available Today</h1>
     </div>
   )
 }
