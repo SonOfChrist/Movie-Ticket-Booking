352edeecb2b746ceb1c57383af8066620468e06d
SoftwareDev Stan <mochogestanley80@gmail.com>
2025-07-18 10:42:22 +0300

    MoviesPage-Structure

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -8,7 +8,10 @@ const Movies = () => {
     <div className='relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
       <BlurCircle top='150px' left='0px' />
       <BlurCircle bottom='50px' right='50px' />
-      <h1 className='text-lg font-medium my-4'>Now Showing | Buy Your Tickect, Don't miss Out</h1>
+      <h1 className='text-3xl md:text-4xl font-extrabold text-center mb-10 bg-gradient-to-r from-primary via-pink-500 to-white bg-clip-text text-transparent tracking-tight drop-shadow-lg'>
+        Now Showing – Buy Your Ticket &amp; Experience the Magic of Movies! 
+      </h1>
+      <div className="h-1 w-full max-w-4xl mx-auto mb-10 rounded-full bg-gradient-to-r from-primary via-pink-500 to-white opacity-80"></div>
       <div className='flex flex-wrap max-sm:justify-center gap-14'>
         {dummyShowsData.map((movie) => (
           <MovieCard key={movie._id} movie={movie} />
